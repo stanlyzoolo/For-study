65f26ab9154f1adf94663ab40d46d8f90894700a
Stanlyzoolo <51911715+Stanlyzoolo@users.noreply.github.com>
2019-11-11 00:24:54 +0300

    Add files via upload

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -43,3 +43,22 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+class Comment (models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = 'Comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__ (self):
+        return 'Комментарий от {} о {}'.format(self.name, self.post)
+
+
+
+
